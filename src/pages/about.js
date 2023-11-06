a0dc2b7d6e9c3043cc110ce20cdb2c4133fa1d13
kimsarahsj <53312812+kimsarahsj@users.noreply.github.com>
2023-11-06 16:42:59 -0500

    edit about page and content positions

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,12 +9,24 @@ export default function About() {
 <main className={`flex flex-col`}>
     <div className={`bg-neutralOne grid grid-cols-1 lg:grid-cols-2 gap-8 px-12 py-12 xl:px-[10%] justify-items-center`}>
       <div className={`order-first lg:order-first flex justify-center items-center`}>
-        <div className={``}> 
-            <Image src={`/images/hi_juna.PNG`}
-              width={400}
-              height={800}
-              alt=""
-            />
+        <div className={'container bg-white pb-6 px-6'}> 
+            <div className={'flex justify-center items-center h-[90px]'}>
+                <Image src={`/images/juna1.jpg`}
+                width={180}
+                height={180}
+                alt=""
+                className={'rounded-full relative top-14'}
+                />
+            </div>
+                <div className={'container mx-auto bg-neutralBrown text-white px-8 py-28'}>
+                    <div className={'font-cocogothic uppercase text-xl text-center'}>Hello,</div>
+                    <div className={'font-cocogothic uppercase text-xl text-center'}> My Name is</div>
+                    <div className={'font-catchymager uppercase text-6xl text-center'}>Juna</div>
+                    <div className={'leading-6 text-cocogothic'}>I&apos;m delighted to share a glimpse of my journey with you.</div>
+                    <div className={'leading-6 text-cocogothic'}>I wear many hats in life, but at my core, I&apos;m a passionate artist and a devoted wife and mother to two wonderful children.</div>
+                    <div className={'leading-6 text-cocogothic'}>My artistic journey began as a painter, and I&apos;ve been creating freelance art for as long as I can remember.</div>
+                    <div className={'leading-6 text-cocogothic'}>My academic background in Communication Science and Journalism, along with my experience as a marketing specialist, has given me a distinctive viewpoint for my business.</div>
+                </div>
       </div>
       </div>
         <div className="w-[80%] xl:w-[90%]">
